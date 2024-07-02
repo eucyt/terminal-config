a2d1eb4c8a70241a11ad99fcfd5658839a018225
yuta <eypc.ofun@gmail.com>
2024-07-02 23:36:18 +0900

    Fix wezterm design

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,25 +1,25 @@
-require 'format'
-require 'status'
-require 'event'
+require("format")
+require("status")
+require("event")
 
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- colors
 config.color_scheme = "nord"
-config.window_background_opacity = 0.93
+config.window_background_opacity = 1.0
 
 -- font
 config.font = require("wezterm").font("JetBrainsMono Nerd Font Mono")
 config.font_size = 13.0
 config.window_frame = {
-  font = wezterm.font { family ='Roboto', weight = 'Bold' },
-  font_size = 11.0,
+	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+	font_size = 11.0,
 }
 
 -- status
@@ -29,8 +29,7 @@ config.status_update_interval = 1000
 config.window_decorations = "RESIZE"
 
 -- window size
-config.initial_rows = 48
-config.initial_cols = 160
+config.initial_rows = 72
+config.initial_cols = 240
 
 return config
-
